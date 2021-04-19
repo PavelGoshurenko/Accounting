bdee55ce1b11987979841d49c1fa97e26de55a65
PavelGoshurenko <goshurenkopl@gmail.com>
2021-04-19 21:20:01 +0300

    inventories admin

--- goods/admin.py
+++ goods/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from goods.models import Product, ProductCategory, ProductBrand, Invoice, Incoming, Sale, Inventory, Task
 
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('date', 'product', 'shortage', 'confirmed')
+    list_filter = ('confirmed', )
+
+
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(ProductCategory)
@@ -8,5 +14,5 @@ admin.site.register(ProductBrand)
 admin.site.register(Invoice)
 admin.site.register(Incoming)
 admin.site.register(Sale)
-admin.site.register(Inventory)
+admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(Task)
